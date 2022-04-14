f0fbdbf3ad37d8e0ff694cd79fbacf8db8bf06e1
Rustam Atabekov <atabekovrustam102@gmail.com>
2022-04-14 15:46:23 +0300

    Delete createPerson.js

--- main.js
+++ main.js
@@ -2,6 +2,16 @@
 const $btnLeft = document.querySelector(".btn-left");
 const $btnRight = document.querySelector(".btn-right");
 
+let personName;
+let personProfession;
+let personComment;
+
+export function saveComment() {
+  personName = document.querySelector("#personName").value;
+  personProfession = document.querySelector("#personProfession").value;
+  personComment = document.querySelector("#personComment").value;
+}
+
 $btnLeft.addEventListener("click", function () {
   const $currentItem = document.querySelector(".current-item");
   $currentItem.innerHTML = `<div class="item">
@@ -18,18 +28,18 @@ $btnLeft.addEventListener("click", function () {
               </div>`;
 });
 
-// $btnRight.addEventListener("click", function () {
-//   const $currentItem = document.querySelector(".current-item");
-//   $currentItem.innerHTML = `<div class="item">
-//                 <picture">
-//                   <img src="pictures/testimonials/" alt="picture" />
-//                 </picture>
-//                 <p>
-//                   ${personComment}
-//                 </p>
-//                 <figcaption>
-//                   <p class="name-person">${personName}</p>
-//                   <p class="profession">${personProfession}</p>
-//                 </figcaption>
-//               </div>`;
-// });
+$btnRight.addEventListener("click", function () {
+  const $currentItem = document.querySelector(".current-item");
+  $currentItem.innerHTML = `<div class="item">
+                <picture">
+                  <img src="pictures/testimonials/" alt="picture" />
+                </picture>
+                <p>
+                  ${personComment}
+                </p>
+                <figcaption>
+                  <p class="name-person">${personName}</p>
+                  <p class="profession">${personProfession}</p>
+                </figcaption>
+              </div>`;
+});
