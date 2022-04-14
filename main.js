f3cc8235d40a6819b9b133026e950a05ec89b4ee
Rustam Atabekov <atabekovrustam102@gmail.com>
2022-04-14 15:17:21 +0300

    Continue to work with slider

--- main.js
+++ main.js
@@ -2,8 +2,7 @@
 const $btnLeft = document.querySelector(".btn-left");
 const $btnRight = document.querySelector(".btn-right");
 
-const personName = document.querySelector(".input-name").value;
-const personProfession = $btnLeft.addEventListener("click", function () {
+$btnLeft.addEventListener("click", function () {
   const $currentItem = document.querySelector(".current-item");
   $currentItem.innerHTML = `<div class="item">
                 <picture">
@@ -19,18 +18,18 @@ const personProfession = $btnLeft.addEventListener("click", function () {
               </div>`;
 });
 
-$btnRight.addEventListener("click", function () {
-  const $currentItem = document.querySelector(".current-item");
-  $currentItem.innerHTML = `<div class="item">
-                <picture">
-                  <img src="pictures/testimonials/" alt="picture" />
-                </picture>
-                <p>
-                  ${personComment}
-                </p>
-                <figcaption>
-                  <p class="name-person">${personName}</p>
-                  <p class="profession">${personProfession}</p>
-                </figcaption>
-              </div>`;
-});
+// $btnRight.addEventListener("click", function () {
+//   const $currentItem = document.querySelector(".current-item");
+//   $currentItem.innerHTML = `<div class="item">
+//                 <picture">
+//                   <img src="pictures/testimonials/" alt="picture" />
+//                 </picture>
+//                 <p>
+//                   ${personComment}
+//                 </p>
+//                 <figcaption>
+//                   <p class="name-person">${personName}</p>
+//                   <p class="profession">${personProfession}</p>
+//                 </figcaption>
+//               </div>`;
+// });
