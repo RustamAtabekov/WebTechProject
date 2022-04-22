191741f8d40a91cd3fdb1f567061a6e9a56ea50a
Rustam Atabekov <atabekovrustam102@gmail.com>
2022-04-22 16:58:27 +0300

    Continue work with likes

--- main.js
+++ main.js
@@ -40,72 +40,16 @@ $btnRight.addEventListener("click", function () {
   }
 });
 
-// get LIKE element
-const $like = document.querySelector(".like");
-let imgLike = $like.children;
-
-// get sum(counter) LIKES
-const sumLikes = document.querySelector(".sum-likes");
-sumLikes.children;
-
-// set initial amount of counter LIKES
-let counterLikes = 0;
-sumLikes.innerHTML = counterLikes;
-
-// increase or decrease LIKES by pressing buttons
-$like.addEventListener("click", function () {
-  let i = 0;
-  if (
-    imgLike[i].src.includes("pictures/testimonials/like-off.jpg") &&
-    imgDislike[i].src.includes("pictures/testimonials/dislike-off.jpg")
-  ) {
-    imgLike[i].src = "pictures/testimonials/like-on.jpg";
-    sumLikes.innerHTML = ++counterLikes;
-  } else if (
-    imgLike[i].src.includes("pictures/testimonials/like-off.jpg") &&
-    imgDislike[i].src.includes("pictures/testimonials/dislike-on.jpg")
-  ) {
-    imgLike[i].src = "pictures/testimonials/like-on.jpg";
-    sumLikes.innerHTML = ++counterLikes;
-    imgDislike[i].src = "pictures/testimonials/dislike-off.jpg";
-    sumDislikes.innerHTML = --counterDislikes;
-  } else {
-    imgLike[i].src = "pictures/testimonials/like-off.jpg";
-    sumLikes.innerHTML = --counterLikes;
-  }
-});
-
-// get DISLIKE element
-const $dislike = document.querySelector(".dislike");
-let imgDislike = $dislike.children;
-
-// get sum(counter) DISLIKES
-const sumDislikes = document.querySelector(".sum-dislikes");
-sumDislikes.children;
-
-// set initial amount of counter DISLIKES
-let counterDislikes = 0;
-sumDislikes.innerHTML = counterDislikes;
-
-// increase or decrease DISLIKES by pressing buttons
-$dislike.addEventListener("click", function () {
-  let i = 0;
-  if (
-    imgDislike[i].src.includes("pictures/testimonials/dislike-off.jpg") &&
-    imgLike[i].src.includes("pictures/testimonials/like-off.jpg")
-  ) {
-    imgDislike[i].src = "pictures/testimonials/dislike-on.jpg";
-    sumDislikes.innerHTML = ++counterDislikes;
-  } else if (
-    imgDislike[i].src.includes("pictures/testimonials/dislike-off.jpg") &&
-    imgLike[i].src.includes("pictures/testimonials/like-on")
-  ) {
-    imgDislike[i].src = "pictures/testimonials/dislike-on.jpg";
-    sumDislikes.innerHTML = ++counterDislikes;
-    imgLike[i].src = "pictures/testimonials/like-off.jpg";
-    sumLikes.innerHTML = --counterLikes;
-  } else {
-    imgDislike[i].src = "pictures/testimonials/dislike-off.jpg";
-    sumDislikes.innerHTML = --counterDislikes;
-  }
+let likeElements = document.querySelectorAll(".like");
+likeElements.forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    e.preventDefault();
+    item.src = item.querySelector("img").getAttribute("src");
+    if ((item.src = "pictures/testimonials/like-off.jpg")) {
+      item.src.remove;
+      item.in;
+      item.src = "pictures/testimonials/like-on.jpg";
+      console.log(item.src);
+    }
+  });
 });
